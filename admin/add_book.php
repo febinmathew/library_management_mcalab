c74debaf915fa276f8ffbfcda38284e6689a0703
mca 1865 <fmfebinmathew@gmail.com>
2019-10-24 15:38:41 +0530

    updates

--- admin/add_book.php
+++ admin/add_book.php
@@ -7,31 +7,47 @@
 <body>
 
 <center>
+<?php 
+echo "<h2>New book created successfully</h2>";
+if ($_POST["book_name"]){
+
+
+    $sql = "INSERT INTO books (book_name, book_author, book_publisher,book_price,book_pages)
+    VALUES ('test', 'tes', 'test', 2, 3)";
+if ($conn->query($sql) === TRUE) {
+    echo "<h2>New book created successfully</h2>";
+} else {
+    echo "Error: " . $sql . "<br>" . $conn->error;
+}
+} else{ ?>
+
+
 <h2>Enter your book details</h2>
 <form>
 <table>
 	<tr>
-		<td>Book Name</td><td><input type="text" name="bkname"></td>
+		<td>Book Name</td><td><input type="text" name="book_name"></td>
 	</tr>
 	<tr>
-		<td>Author</td><td><input type="text" name="bkautr"></td>
+		<td>Author</td><td><input type="text" name="book_author"></td>
 	</tr>
 	<tr>
-		<td>Publisher</td><td><input type="text" name="bkpub"></td>
+		<td>Publisher</td><td><input type="text" name="book_publisher"></td>
 	</tr>
 	<tr>
 		<td>Published Date</td><td><input type="text" name="pubdat"></td>
 	</tr>
 	<tr>
-		<td>Pages</td><td><input type="text" name="bkpage"></td>
+		<td>Pages</td><td><input type="text" name="book_pages"></td>
 	</tr>
 	<tr>
-		<td>Price</td><td><input type="text" name="bkprice"></td>
+		<td>Price</td><td><input type="text" name="book_price"></td>
 	</tr>
 	
 </table>
 <input type="submit" name="submit" value="SUBMIT">
 </form>
+<?php } ?>
 </center>
 
 </body>
--- sql_connection.php
+++ sql_connection.php
@@ -2,7 +2,9 @@
 $servername = "localhost";
 $username = "root";
 $password = "cetmca";
-$tablename_books="books";
+
+$table_books="books";
+$t_books_name="name";
 
 // Create connection
 $conn = new mysqli($servername, $username, $password);
