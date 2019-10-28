150a5fe3fd1c4045d7e7a8bef36bdf4454ec385c
mca 1865 <fmfebinmathew@gmail.com>
2019-10-28 11:35:13 +0530

    sql conenction

--- admin/add_book.php
+++ admin/add_book.php
@@ -8,13 +8,13 @@
 
 <center>
 <?php 
-echo "<h2>New book created successfully</h2>";
+echo "name in post : ".$_POST["book_name"];
 if ($_POST["book_name"]){
+    $sql = "INSERT INTO books (book_name, book_author, book_publisher,book_publish_date,book_price,book_pages)
+    VALUES ('".$_POST["book_name"]."', '".$_POST["book_author"]."', '".$_POST["book_publisher"]."','".$_POST["book_publish_date"]."',".$_POST["book_price"]." , ".$_POST["book_pages"].")";
 
-
-    $sql = "INSERT INTO books (book_name, book_author, book_publisher,book_price,book_pages)
-    VALUES ('test', 'tes', 'test', 2, 3)";
 if ($conn->query($sql) === TRUE) {
+
     echo "<h2>New book created successfully</h2>";
 } else {
     echo "Error: " . $sql . "<br>" . $conn->error;
@@ -23,25 +23,25 @@ if ($conn->query($sql) === TRUE) {
 
 
 <h2>Enter your book details</h2>
-<form>
+<form method="POST">
 <table>
 	<tr>
-		<td>Book Name</td><td><input type="text" name="book_name"></td>
+		<td>Book Name</td><td><input type="text" name="book_name" required></td>
 	</tr>
 	<tr>
-		<td>Author</td><td><input type="text" name="book_author"></td>
+		<td>Author</td><td><input type="text" name="book_author" required></td>
 	</tr>
 	<tr>
-		<td>Publisher</td><td><input type="text" name="book_publisher"></td>
+		<td>Publisher</td><td><input type="text" name="book_publisher" required></td>
 	</tr>
 	<tr>
-		<td>Published Date</td><td><input type="text" name="pubdat"></td>
+		<td>Published Date</td><td><input type="date" name="book_publish_date" required></td>
 	</tr>
 	<tr>
-		<td>Pages</td><td><input type="text" name="book_pages"></td>
+		<td>Pages</td><td><input type="text" name="book_pages" required></td>
 	</tr>
 	<tr>
-		<td>Price</td><td><input type="text" name="book_price"></td>
+		<td>Price</td><td><input type="text" name="book_price" requiredy></td>
 	</tr>
 	
 </table>
--- sql_connection.php
+++ sql_connection.php
@@ -2,12 +2,13 @@
 $servername = "localhost";
 $username = "root";
 $password = "cetmca";
+$db_name = "library_cet";
 
 $table_books="books";
 $t_books_name="name";
 
 // Create connection
-$conn = new mysqli($servername, $username, $password);
+$conn = new mysqli($servername, $username, $password,$db_name);
 
 // Check connection
 if ($conn->connect_error) {
